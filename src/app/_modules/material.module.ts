f7eec344da3b77647f86fea401b8c50ba61aaf22
tomanugarh <tomanugrah@gmail.com>
2019-06-26 15:34:22 +0700

    add topnav and header in shop-page2

--- src/app/_modules/material.module.ts
+++ src/app/_modules/material.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatCardModule, MatIconModule, MatDividerModule,
   MatFormFieldModule, MatInputModule, MatToolbarModule, MatSidenavModule,
-  MatMenuModule, MatBadgeModule, MatSelectModule, MatListModule} from '@angular/material';
+  MatMenuModule, MatBadgeModule, MatSelectModule, MatListModule, MatGridListModule} from '@angular/material';
 
 @NgModule({
   declarations: [],
@@ -13,13 +13,13 @@ import {MatButtonModule, MatCheckboxModule, MatCardModule, MatIconModule, MatDiv
     BrowserAnimationsModule,
     MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatToolbarModule,
     MatCardModule, MatIconModule, MatDividerModule, MatSidenavModule,
-    MatMenuModule, MatBadgeModule, MatSelectModule, MatListModule,
+    MatMenuModule, MatBadgeModule, MatSelectModule, MatListModule, MatGridListModule
   ],
   exports: [
     BrowserAnimationsModule,
     MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatToolbarModule,
     MatCardModule, MatIconModule, MatDividerModule, MatSidenavModule,
-    MatMenuModule, MatBadgeModule, MatSelectModule, MatListModule,
+    MatMenuModule, MatBadgeModule, MatSelectModule, MatListModule, MatGridListModule
   ]
 })
 export class MaterialModule { }
