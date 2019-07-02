5177c4558400e0933c77bdf42f728f0ba33f001d
Mr Hafid <achmadhafid668@gmail.com>
2019-07-02 15:37:45 +0700

    card fix

--- src/app/landing/custom-page/cardh/cardup/cardup.component.ts
+++ src/app/landing/custom-page/cardh/cardup/cardup.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CardupComponent implements OnInit {
   cards = [  
     { title: 'Pitek', detl: 'Visitor Online',  images: 'https://material.angular.io/assets/img/examples/shiba1.jpg' },  
-    { title: 'Ayam', detl: 'Total Sales', cols: 1, rows: 1 },  
-    { title: 'Maya', detl: 'Total Project', cols: 1, rows: 2 },  
-    { title: 'Dia', detl: 'Today Income', cols: 1, rows: 1 }  
+    { title: 'Ayam', detl: 'Total Sales', images: 'https://material.angular.io/assets/img/examples/shiba2.jpg' },  
+    { title: 'Maya', detl: 'Total Project', images: 'https://material.angular.io/assets/img/examples/shiba1.jpg' },  
+    { title: 'Dia', detl: 'Today Income', images: 'https://material.angular.io/assets/img/examples/shiba1.jpg' }  
   ];
   constructor() { }
 
